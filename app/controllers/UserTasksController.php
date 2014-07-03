8bf71ea88a5f21988fa18aad26e81ba0cc4de704
bmitch <wkmitch@gmail.com>
2014-07-02 20:02:52 -0400

    After episode 5

---
+++ app/controllers/UserTasksController.php
@@ -0,0 +1,22 @@
+<?php
+
+class UserTasksController extends BaseController
+{
+	public function index($username)
+	{
+
+		$tasks = Task::byUsername($username);
+
+
+		return View::make('tasks.index', compact('tasks'));
+	}
+
+	public function show($username, $taskId)
+	{
+		// grab the task by id ssociated with user
+
+		$task = Task::find($taskId, $username);
+
+		return View::make('tasks.show', compact('task'));
+	}
+}
--- app/routes.php
+++ app/routes.php
@@ -4,7 +4,11 @@ Route::get('/', ['as' => 'home', 'uses' => 'TasksController@index']);
 
 Route::get('tasks/{id}', 'TasksController@show')->where('id', '\d+');
 
-Route::get('{username}/tasks', function($username)
+Route::get('{username}/tasks', 'UserTasksController@index');
+
+Route::get('{username}/tasks/{id}', ['as' => 'user.tasks.show', 'uses' => 'UserTasksController@show']);
+
+/*Route::get('{username}/tasks', function($username)
 {
 	$user = User::whereUsername($username)->first();
 	return $user->tasks;
@@ -23,3 +27,4 @@ Route::get('{username}/tasks/{id}', function($username, $id)
 	return View::make('tasks.show', compact('user' ,'task'));
 	// load a view to display it
 });	
+*/
